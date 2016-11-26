a4d6320de1cc66d8be1f1fd4b5e0dad40fe2e8dd
prashanthkumar97 <mailmeatparashanth@gmail.com>
2016-11-26 20:17:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-  user:'vishnuclu',
-  database:'vishnuclu',
+  user:'prashanthkumar97',
+  database:'prashanthkumar97',
   host:'db.imad.hasura-app.io',
   port:'5432',
   password: process.env.DB_PASSWORD
